50557eec19a4231d81be8cbc4743e23da6302d28
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 10:48:25 -0400

    updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,14 +8,16 @@ export default class Home extends React.Component {
     render() {
         return (
             <div className = "main-container">
-                <div className = "main-header">
-                <h1>Welcome to Potato</h1>
+                <div className = "main-card">
+                    <div className = "main-header">
+                    <h1>Welcome to Potato</h1>
+                    </div>
+                    <div className = "main-subheader">
+                        <h2>The best source for easy meal recipes.</h2>
+                    </div>
+                    <Button variant="dark">Search Recipes</Button>{' '}
+                    <Button variant="outline-success">Get Started</Button>{' '}
                 </div>
-                <div className = "main-subheader">
-                    <h2>The best source for easy meal recipes.</h2>
-                </div>
-                <Button variant="dark">Search Recipes</Button>{' '}
-                <Button variant="outline-success">Get Started</Button>{' '}
             </div>
         )
     }
