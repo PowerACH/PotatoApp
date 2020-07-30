767ca80efd2bafb5612e344b0f9f8e4b83dadda1
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 10:39:33 -0400

    updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from 'react-bootstrap/button'
+import { Button } from 'react-bootstrap'
+import '../App.css'
 
 
 
@@ -8,12 +9,12 @@ export default class Home extends React.Component {
         return (
             <div className = "main-container">
                 <div className = "main-header">
-                <h1>Welome to Potato</h1>
+                <h1>Welcome to Potato</h1>
                 </div>
                 <div className = "main-subheader">
                     <h2>The best source for easy meal recipes.</h2>
                 </div>
-                <Button variant="light">Light</Button> <Button variant="dark">Search Recipes</Button>{' '}
+                <Button variant="dark">Search Recipes</Button>{' '}
                 <Button variant="outline-success">Get Started</Button>{' '}
             </div>
         )
