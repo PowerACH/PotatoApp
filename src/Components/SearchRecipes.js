72cb7c49559e78adc3e906470f8f2a021ea59f52
Rachel Powe <rachelpowe@gmail.com>
2020-07-31 16:18:03 -0400

    deleted page

--- src/Components/SearchRecipes.js
+++ src/Components/SearchRecipes.js
@@ -1,33 +1,80 @@
 import React from 'react';
 import '../App.css';
-import SearchBar from './SearchBar'
-import Axios from 'axios';
+import { InputGroup, FormControl, Button} from 'react-bootstrap'
+import axios from 'axios'
 
-const BASE_API = "https://www.themealdb.com/api/json/v1/1"
-
-export default class SearchRecipes extends React.Component {
+//Search bar for Search Recipes component
+export default class SearchBar extends React.Component {
     constructor() {
         super();
         this.state = {
-            recipes: []
+            keyword: "", //search value
+            recipe: {}, //recipe results
+            count: 0 //number of results found
         }
     }
 
-    componentDidMount() {
-        Axios.get(`${BASE_API}/randomselection.php`)
-        .then(res=> this.setState({ recipes: res.data }))
-        .catch(error => console.error( error.message ));
+    componentDidMount = () => { 
+        document.addEventListener('keypress', this.enter);//makes the enter key listener active 
     }
 
-    render() {
-        console.log(this.state)
-        return (
-            <div>
-                <SearchBar />
+    componentWillUnmount() {
+        document.removeEventListener('keypress', this.enter); //deactivates the key listener
+    }
 
-                
-            </div>
-            
+    //a function for search bar to search
+    handleSearch = event => {
+        event.preventDefault();
+        console.log(this.state.keyword)
+        
+        axios.get('https://www.themealdb.com/api/json/v1/1/search.php?s=' + this.state.keyword)
+        .then((data => {
+            // console.log(data.data.meals);
+            this.setState({recipe: data.data.meals}) //change state of recipe 
+            this.setState({count: this.state.recipe.length}) //change state of count
+            // console.log(this.state)
+        }))
+        
+        .catch(error => {
+            console.error( error.message );
+        })
+    }
+    //a function to assign value to keyword
+    handleChange = event => {
+        this.setState({[event.target.id]: event.target.value
+        }); 
+    }
+
+
+    render() {
+                    console.log(this.state)
+        return (
+        <div>
+            {/* //Search Bar (Bootstrap) */}
+        <InputGroup size="lg" className="mb-3" >
+            <FormControl
+              id = "keyword"
+              placeholder="enter keyword"
+              aria-describedby="basic-addon2"
+              autoComplete="list"
+              onChange={this.handleChange} 
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary" onClick={this.handleSearch}>Search Recipes</Button>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <div>
+                <h1>hello</h1>
+          </div>
+          </div>
         )
+    }
+    
+    //Function for the ENTER key listener 
+    enter = target => {
+        if(target.charCode === 13) {
+            this.handleSearch(target) 
+        }
     }
 }
