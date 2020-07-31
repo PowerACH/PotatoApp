4e937ccb79067fa8dc8518dd1531dc7ae1ec7009
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 21:21:27 -0400

    updates

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,6 +1,45 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import { InputGroup, FormControl, Button} from 'react-bootstrap'
+
+//Search bar for Search Recipes component
+export default class SearchBar extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            keyword: ""
+        }
+    }
+
+    //create a function for search bar to search
+    handleSearch = event => {
+        console.log(this.state)
+        event.preventDefault();
+    }
+    //create a function to assign value to keyword
+    handleChange = event => {
+        this.setState({
+            [event.target.id]: event.target.value
+           
+        }); 
+    }
 
 
-//Search bar for the SearchRecipes component
-
-const SearchBar
+    render() {
+        return (
+            //Search Bar (Bootstrap)
+        <InputGroup size="lg" className="mb-3" >
+            <FormControl
+              id = "keyword"
+              placeholder="enter keyword"
+              aria-describedby="basic-addon2"
+              autoComplete="list"
+              onChange={this.handleChange} 
+            />
+            <InputGroup.Append>
+              <Button variant="outline-secondary" onClick={this.handleSearch}>Search Recipes</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        )
+    }
+}
--- src/Components/SearchRecipes.js
+++ src/Components/SearchRecipes.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import '../App.css';
-import { InputGroup, FormControl, Button} from 'react-bootstrap'
-
+import SearchBar from './SearchBar'
 export default class SearchRecipes extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            keyword: ""
-        }
-    }
-
-    //create a function for search bar to search
-    handleSearch = event => {
-        console.log(this.state)
-        event.preventDefault();
-    }
-    //create a function to assign value to keyword
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-           
-        }); 
-    }
 
 
     render() {
         return (
             //Search Bar (Bootstrap)
-        <InputGroup size="lg" className="mb-3" >
-            <FormControl
-              id = "keyword"
-              placeholder="enter keyword"
-              aria-describedby="basic-addon2"
-              autoComplete="list"
-              onChange={this.handleChange}
-            />
-            <InputGroup.Append>
-              <Button variant="outline-secondary" onClick={this.handleSearch}>Search Recipes</Button>
-            </InputGroup.Append>
-          </InputGroup>
+            <SearchBar />
         )
     }
 }
