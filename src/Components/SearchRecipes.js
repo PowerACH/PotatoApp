f36f6a02f59adf375d60ce47ff621f9c564b1a9f
Rachel Powe <rachelpowe@gmail.com>
2020-07-30 21:54:25 -0400

    updates

--- src/Components/SearchRecipes.js
+++ src/Components/SearchRecipes.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import '../App.css';
 import SearchBar from './SearchBar'
-export default class SearchRecipes extends React.Component {
+import Axios from 'axios';
 
+const BASE_API = "https://www.themealdb.com/api/json/v1/1"
+
+export default class SearchRecipes extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            recipes: []
+        }
+    }
+
+    componentDidMount() {
+        Axios.get(`${BASE_API}/randomselection.php`)
+        .then(res=> this.setState({ recipes: res.data }))
+        .catch(error => console.error( error.message ));
+    }
 
     render() {
+        console.log(recipes)
         return (
-            //Search Bar (Bootstrap)
-            <SearchBar />
+            <div>
+                <SearchBar />
+
+                
+            </div>
+            
         )
     }
 }
