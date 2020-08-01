330d5c94a726a41ce0ed8bfccf640073e79d0400
Rachel Powe <rachelpowe@gmail.com>
2020-08-01 02:57:11 -0400

    added params

--- src/Components/RecipeCard.js
+++ src/Components/RecipeCard.js
@@ -2,20 +2,19 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class RecipeCard extends Component {
-    state = {
-        id: []
-    }
 
     componentDidMount(){
-        let id = this.props.match.params.id
-        axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-            .then(res => { this.setState({ is: res.data })})
-            console.log(res)
+        const {id} = this.props.match.params;
+        axios.get('https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id)
+            .then(res => { this.setState({ id: res.data })})
+            console.log(id)
     }
     
     render() {
+        console.log(this.state.id.meals)
         return (
             <div className = "single-meal-container">
+            <h1>recipe goes here</h1>
                 <div className = "single-meal-image">
                     <img src={this.strMealThumb}/>
                 </div>
