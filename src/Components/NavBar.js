59b9bf80e713a5ba7bd03ce226fb304cabdd81a7
Rachel Powe <rachelpowe@gmail.com>
2020-08-04 06:55:25 -0400

    deleted button

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,19 +2,9 @@ import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import RecipeCard from './RecipeCard'
 
 export default function NavBar() {
-
-    function handleClick() {
-        fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-            .then(response => response.json())
-            .then(response => response.meals[0].idMeal)
-             
-            .then(response => <Link to ="/recipe/:id" />)
-            .catch(e => {
-                console.error(e);
-            })
-    }
 
     return (
         <div>
@@ -22,8 +12,8 @@ export default function NavBar() {
                 <Navbar.Brand href="/" className="nav-logo mr-auto">potato</Navbar.Brand>
                 <Nav className="nav-logo ml-auto">
                     <Nav.Link href="/">home</Nav.Link>
-                    <Nav.Link href="#" onClick={handleClick}>random meal</Nav.Link>
-                    <Nav.Link href="#">saved</Nav.Link>
+                    <Nav.Link href="/Random">random meal</Nav.Link>
+                    {/* <Nav.Link href="#">saved</Nav.Link> */}
                 </Nav>
             </Navbar>
         </div>
